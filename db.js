6bdd8f538a924abe7867c5178004bf1f925e15e6
sam t <sjtindell@gmail.com>
2015-12-12 21:06:05 -0800

    Added mongoose connection, post model, and updated api for http post

---
+++ db.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/socialapp", function() {
+  console.log("mongodb connected");
+});
+
+module.exports = mongoose;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
-var express = require("express");
 var bodyParser = require("body-parser");
+var express = require("express");
+var Post = require("../models/post");
+
 
 var app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 
 app.get("/api/posts", function(req, res) {
   res.json([
@@ -10,9 +13,22 @@ app.get("/api/posts", function(req, res) {
       username: "sjtindellxxx",
       body: "node rules!"
     }
-  ])
+  ]);
 })
 
+
+app.post("/api/posts", function(req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body: req.body.body
+  });
+  post.save(function(err, post) {
+    if (err) { return next(err) };
+    res.status(201).json(post);
+  });
+})
+
+
 app.listen(3000, function() {
-  console.log("Server listening on", 3000)
+  console.log("Server listening on", 3000);
 })
