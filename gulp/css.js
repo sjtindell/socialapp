d8288cf04e12da2d3707af6cf2ab32bee6e72759
sam t <sjtindell@gmail.com>
2015-12-13 00:31:23 -0800

    Updated gulp files to combine features into one extensible task 'dev'

--- gulp/css.js
+++ gulp/css.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp")
 var stylus = require("gulp-stylus")
 
+
 gulp.task("css", function() {
   gulp.src("css/**/*.styl")
     .pipe(stylus())
     .pipe(gulp.dest("assets"))
+})
+
+
+gulp.task("watch:css", function() {
+  gulp.watch("css/**/*.styl", ["css"])
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var fs = require("fs");
+var gulp = require("gulp")
+var fs = require("fs")
 
 fs.readdirSync(__dirname + "/gulp").forEach(function(task) {
   require("./gulp/" + task)
 })
 
+gulp.task("dev", ["watch:js", "watch:css", "dev:server"])
