0d71b0afe657c0686614a3cf93f4527ade55ca3c
sam t <sjtindell@gmail.com>
2015-12-12 23:28:18 -0800

    Added app service for accessing API. Angular and Node API now fully integrated. App v0.1

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,9 @@
 var app = angular.module("app", [])
 
-app.controller("PostsCtrl", function($scope, $http) {
+app.controller("PostsCtrl", function($scope, PostsSvc) {
   $scope.addPost = function() {
     if ($scope.postBody) {
-      $http.post("/api/posts", {
+      PostsSvc.create({
         username: "sjtindellxxx",
         body: $scope.postBody
       }).success(function(post) {
@@ -12,8 +12,17 @@ app.controller("PostsCtrl", function($scope, $http) {
       })
     }
   }
-  
-  $http.get("/api/posts").success(function(posts) {
+  PostsSvc.fetch().success(function(posts) {
     $scope.posts = posts
   })
+})
+
+
+app.service("PostsSvc", function($http) {
+  this.fetch = function() {
+    return $http.get("/api/posts")
+  }
+  this.create = function(post) {
+    return $http.post("/api/posts", post)
+  }
 })
