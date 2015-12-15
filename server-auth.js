c91d1847df845a455c5d84f68745aa484d0f3ebd
sam t <sjtindell@gmail.com>
2015-12-14 17:53:20 -0800

    Added mongo user model for storing passwords, encrypted using bcrypt

--- server-auth.js
+++ server-auth.js
@@ -1,40 +1,48 @@
 var express = require("express");
 var jwt = require("jwt-simple");
-var _ = require("lodash");
-
-
+var bcrypt = require("bcrypt");
 var app = express();
+var User = require("./models/user");
 app.use(require("body-parser").json());
 
 
 var secretKey = "supersecretkey";
-var users = [{username: "sjtindellxxx", password: "pass"}];
-
-
-function findUser(username) {
-  return _.find(users, {username: username});
-};
-
-function validateUser(user, password) {
-  return user.password == password;
-};
 
 
 app.post("/session", function(req, res) {
-  var user = findUser(req.body.username);
-  if (!validateUser(user, req.body.password)) {
-    return res.send(401) // Unauthorized
-  }
-  var token = jwt.encode({username: user.username}, secretKey);
-  res.json(token);
+  User.findOne({username: req.body.username})
+  .select("password")
+  .exec(function(err, user) {
+    if (err) { return next(err) };
+    if (!user) { return res.status(401).send() };
+    bcrypt.compare(req.body.password, user.password, function(err, valid) {
+      if (err) { return next(err) };
+      if (!valid) { return res.status(401).send() };
+      var token = jwt.encode({username: user.username}, secretKey);
+      res.json(token);
+    });
+  });
 });
 
 
 app.get("/user", function(req, res) {
   var token = req.headers["x-auth"];
-  var user = jwt.decode(token, secretKey);
-  // TODO: pull user info from database
-  res.json(user);
+  var auth = jwt.decode(token, secretKey);
+  User.findOne({username: auth.username}, function(err, user) {
+    res.json(user);
+  });
+});
+
+
+app.post("/user", function(req, res, next) {
+  var user = new User({username: req.body.username});
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    user.password = hash;
+    user.save(function(err, user) {
+      if (err) { throw next(err) };
+      res.status(201).send()
+    });
+  });
 });
 
 
