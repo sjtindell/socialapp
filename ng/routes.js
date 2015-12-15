919086fb06db6157e9741d44ae11ae1202bb5bd5
sam t <sjtindell@gmail.com>
2015-12-14 19:28:21 -0800

    Added html5 pushstate to remove '#' from urls

--- ng/routes.js
+++ ng/routes.js
@@ -1,5 +1,9 @@
 angular.module("app")
-.config(function($routeProvider) {
+.config(function($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  })
   $routeProvider
   .when("/", {controller: "PostsCtrl", templateUrl: "posts.html"})
   .when("/register", {controller: "RegisterCtrl", templateUrl: "register.html"})
