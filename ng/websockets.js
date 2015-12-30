b3d38ea5228f13c221d65e36234df1bd913e760a
sam t <sjtindell@gmail.com>
2015-12-30 14:32:21 -0800

    Added basic websocket connection to client-side app

---
+++ ng/websockets.js
@@ -0,0 +1,8 @@
+angular.module("app")
+.run(function($rootScope) {
+  var url = "ws://localhost:3000"
+  var connection = new WebSocket(url)
+  connection.onopen = function() {
+    console.log("WebSocket connected")
+  }
+})
