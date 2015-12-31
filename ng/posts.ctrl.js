cdca182a24e926f7affe6c3aa9fc3bf9eed114da
sam t <sjtindell@gmail.com>
2015-12-31 11:42:44 -0800

    Added listening callback to posts controller to update page on new post through websocket

--- ng/posts.ctrl.js
+++ ng/posts.ctrl.js
@@ -1,18 +1,26 @@
 angular.module("app")
 .controller("PostsCtrl", function($scope, PostsSvc) {
+  
   $scope.addPost = function() {
     if ($scope.postBody) {
       PostsSvc.create({
-        username: "sjtindellxxx",
+        username: "default",
         body: $scope.postBody
       }).success(function(post) {
-        $scope.posts.unshift(post)
         $scope.postBody = null
       })
     }
   }
 
-  PostsSvc.fetch().success(function(posts) {
+  PostsSvc.fetch()
+  .success(function(posts) {
     $scope.posts = posts
-  }) 
+  })
+
+  $scope.$on("ws:new_post", function(_, post) {
+    $scope.$apply(function() {
+      $scope.posts.unshift(post)
+    })
+  })
+
 })
