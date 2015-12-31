2ec2a9057b8a4c7b8c682a381c4c854dc7ee79b4
sam t <sjtindell@gmail.com>
2015-12-31 12:04:30 -0800

    Updated client websocket code to attempt reconnect after 10 secs on DC

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,15 +1,23 @@
 angular.module("app")
-.run(function($rootScope) {
-  var url = "ws://localhost:3000"
-  var connection = new WebSocket(url)
-  
-  connection.onopen = function() {
-    console.log("WebSocket connected")
-  }
+.run(function($rootScope, $timeout) {
+  (function connect() {
+    var url = "ws://localhost:3000"
+    var connection = new WebSocket(url)
+    
+    connection.onopen = function() {
+      console.log("WebSocket connected")
+    }
 
-  connection.onmessage = function(e) {
-    console.log(e)
-    var payload = JSON.parse(e.data)
-    $rootScope.$broadcast("ws:" + payload.topic, payload.data)
-  }
+    connection.onclose = function(e) {
+      console.log("WebSocket closed. Reconnecting...")
+      $timeout(connect, 10 * 1000)
+    }
+  
+    connection.onmessage = function(e) {
+      console.log(e)
+      var payload = JSON.parse(e.data)
+      $rootScope.$broadcast("ws:" + payload.topic, payload.data)
+    }
+
+  })()
 })
